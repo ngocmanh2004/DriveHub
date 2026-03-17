5d68411ba2d9c2c3a5a85ed902c8d01754d0b7f8
unknown <khavy>
2026-03-17 23:47:34 +0700

    'add'

--- frontend/src/core/config/environment.ts
+++ frontend/src/core/config/environment.ts
@@ -20,8 +20,8 @@ export const ENVIRONMENT_CONFIGS: Record<BuildEnvironment, EnvironmentConfig> = 
     WS_BASE_URL: 'ws://localhost:8080/ws/student-status',
   },
   production: {
-    API_BASE_URL: process.env.REACT_APP_API_URL || 'http://167.172.78.124:8080',
-    WS_BASE_URL: `${(process.env.REACT_APP_API_URL || 'http://167.172.78.124:8080').replace(/^http/, 'ws')}/ws/student-status`,
+    API_BASE_URL: process.env.REACT_APP_API_URL || 'https://www.driverhub.io.vn',
+    WS_BASE_URL: `${(process.env.REACT_APP_API_URL || 'https://www.driverhub.io.vn').replace(/^http/, 'ws')}/ws/student-status`,
   },
   buildlocal: {
     API_BASE_URL: 'http://192.168.1.99:8080',
